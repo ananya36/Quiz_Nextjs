22612d0e342a181bf7c2671b2ab36feaaac034e7
ananya36 <ananyabundela23@gmail.com>
2024-08-20 10:07:04 +0530

    Added questions to data.js

--- quiz_app/app/data.js
+++ quiz_app/app/data.js
@@ -31,6 +31,36 @@ export const quiz = {
         answers: ['Steve Jobs', 'Bill Gates', 'Elon Musk', 'Mark Zuckerberg'],
         correctAnswer: 'Bill Gates',
       },
+      {
+        id: 6,
+        question: 'In which year did the Titanic sink?',
+        answers: ['1912', '1905', '1920', '1898'],
+        correctAnswer: '1912',
+      },
+      {
+        id: 7,
+        question: 'What is the chemical symbol for oxygen?',
+        answers: ['Fe', 'Au', 'O', 'C'],
+        correctAnswer: 'O',
+      },
+      {
+        id: 8,
+        question: 'Which is the smallest prime number?',
+        answers: ['5', '1', '3', '2'],
+        correctAnswer: '2',
+      },
+      {
+        id: 9,
+        question: 'Which element is liquid at room temperature?',
+        answers: ['Sodium', 'Magnesium', 'Aluminium', 'Carbon'],
+        correctAnswer: 'Sodium',
+      },
+      {
+        id:10,
+        question: 'Which is the largest ocean on Earth?',
+        answers: ['Indian', 'Pacific', 'Atlantic', 'Southern'],
+        correctAnswer: 'Pacific',
+      },
     ],
   };
   
